b83fac620134591ead1ac98051cd7843da14125e
cgarrigoza97 <31805380+cgarrigoza97@users.noreply.github.com>
2026-05-06 21:29:15 -0300

    add endpoints

--- code/backend/Inventory.Api/Controllers/ProductsController.cs
+++ code/backend/Inventory.Api/Controllers/ProductsController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Inventory.Api.Contracts.Requests;
+using Inventory.Api.Contracts.Responses;
 
 namespace Inventory.Api.Controllers
 {
-    [Route("api/products"),ApiController]
+    [Route("api/products"), ApiController]
     public class ProductsController : ControllerBase
     {
+        [HttpGet("{id:guid}")]
+        public Task<ProductResponse?> GetByIdAsync(Guid id)
+            => throw new NotImplementedException();
+
+        [HttpPatch("{id:guid}/stock")]
+        public Task<bool> AdjustStockAsync(Guid id, AdjustStockRequest request)
+            => throw new NotImplementedException();
     }
 }
